daf45181a7c011aa66c649c66175578d03fc034b
kake <gusuncheng@gmail.com>
2017-11-24 19:24:42 +0800

    modi main page style

--- public/js/main.js
+++ public/js/main.js
@@ -657,6 +657,9 @@ socket.on('start', function(data) {
 	if (data.not_found) showInfoMessage("Unit not found");
 	notLastUnitDown = true;
 	if (bWaitingForHighlightNode) bWaitingForHighlightNode = false;
+	if (!notLastUnitUp || (location.hash.length != 33)) {
+		highlightNode(data.nodes[0].data.unit);
+	}
 });
 
 socket.on('next', function(data) {
@@ -714,7 +717,7 @@ function generateMessageInfo(messages, transfersInfo, outputsUnit, assocCommissi
 				'<div class="message">' +
 				'<div class="message_app infoTitleChild" onclick="showHideBlock(event, \'message_' + blockId + '\')">';
 			if (message.app == 'payment') {
-				messagesOut += message.app.substr(0, 1).toUpperCase() + message.app.substr(1) + ' in ' + (asset == 'null' ? 'bytes' : asset);
+				messagesOut += message.app.substr(0, 1).toUpperCase() + message.app.substr(1) + ' in ' + (asset == 'null' ? 'notes' : asset);
 			}
 			else if (message.app == 'asset') {
 				messagesOut += 'Definition of new asset';
@@ -748,7 +751,7 @@ function generateMessageInfo(messages, transfersInfo, outputsUnit, assocCommissi
 								key = input.from_main_chain_index + '_' + input.to_main_chain_index;
 								var objName = (input.type === 'headers_commission' ? 'headers' : (input.type === 'witnessing' ? 'witnessing' : false));
 								if (objName) {
-									messagesOut += '<div><span class="numberFormat">' + assocCommissions[objName][key].sum + '</span> bytes of ' + objName + ' commissions on <a href="#' + assocCommissions[objName][key].address + '">' + assocCommissions[objName][key].address + '</a>' +
+									messagesOut += '<div><span class="numberFormat">' + assocCommissions[objName][key].sum + '</span> notes of ' + objName + ' commissions on <a href="#' + assocCommissions[objName][key].address + '">' + assocCommissions[objName][key].address + '</a>' +
 										' from mci ' + assocCommissions[objName][key].from_mci + ' to mci ' + assocCommissions[objName][key].to_mci + '</div>';
 								}
 							}
@@ -906,14 +909,14 @@ function generateTransactionsList(objTransactions, address) {
 					listTransactions += '<div class="transactionUnitListAddress">' +
 						'<div>' + addressOut + ' ' + commissionName + ' commissions from mci ' + objFrom.from_mci +
 						' to mci ' + objFrom.to_mci + '.' +
-						' Sum: <span class="numberFormat">' + objFrom.sum + '</span> bytes</div>' +
+						' Sum: <span class="numberFormat">' + objFrom.sum + '</span> notes</div>' +
 						'</div>';
 				}
 			}
 			else {
 				addressOut = objFrom.address == address ? '<span class="thisAddress">' + objFrom.address + '</span>' : '<a href="#' + objFrom.address + '">' + objFrom.address + '</a>';
 				listTransactions += '<div class="transactionUnitListAddress"><div>' + addressOut + '</div>' +
-					'<div>(<span class="numberFormat">' + objFrom.amount + '</span> ' + (transaction.asset == null ? 'bytes' : transaction.asset) + ')</div></div>';
+					'<div>(<span class="numberFormat">' + objFrom.amount + '</span> ' + (transaction.asset == null ? 'notes' : transaction.asset) + ')</div></div>';
 			}
 		});
 		listTransactions += '</td><td><img width="32" src="' + (transaction.spent ? '/img/red_right2.png' : '/img/green_right2.png') + '"></td><td>';
@@ -922,7 +925,7 @@ function generateTransactionsList(objTransactions, address) {
 			addressOut = _addressTo.address == address ? '<span class="thisAddress">' + _addressTo.address + '</span>' : '<a href="#' + _addressTo.address + '">' + _addressTo.address + '</a>';
 
 			listTransactions += '<div class="transactionUnitListAddress"><div>' + addressOut + '</div>' +
-				'<div>(<span class="numberFormat">' + _addressTo.amount + '</span> ' + (transaction.asset == null ? 'bytes' : transaction.asset) + ', ' +
+				'<div>(<span class="numberFormat">' + _addressTo.amount + '</span> ' + (transaction.asset == null ? 'notes' : transaction.asset) + ', ' +
 				(_addressTo.spent === 0 ? 'not spent' : 'spent in ' + '<a href="#' + _addressTo.spent + '">' + _addressTo.spent + '</a>') +
 				')</div></div>';
 		}
@@ -938,8 +941,8 @@ socket.on('addressInfo', function(data) {
 		lastOutputsROWID = data.newLastOutputsROWID;
 		nextPageTransactionsEnd = data.end;
 		for (var k in data.objBalance) {
-			if (k === 'bytes') {
-				balance += '<div><span class="numberFormat">' + data.objBalance[k] + '</span> bytes</div>';
+			if (k === 'notes') {
+				balance += '<div><span class="numberFormat">' + data.objBalance[k] + '</span> notes</div>';
 			}
 			else {
 				balance += '<div><span class="numberFormat">' + data.objBalance[k] + '</span> of ' + k + '</div>';
@@ -947,7 +950,7 @@ socket.on('addressInfo', function(data) {
 		}
 		if(data.unspent) {
 			data.unspent.forEach(function(row) {
-				listUnspent += '<div><a href="#' + row.unit + '">' + row.unit + '</a> (<span class="numberFormat">' + row.amount + '</span> ' + (row.asset == null ? 'bytes' : row.asset) + ')</div>';
+				listUnspent += '<div><a href="#' + row.unit + '">' + row.unit + '</a> (<span class="numberFormat">' + row.amount + '</span> ' + (row.asset == null ? 'notes' : row.asset) + ')</div>';
 			});
 		}
 		$('#address').html(data.address);
