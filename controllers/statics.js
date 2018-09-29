c968d2dc4c547c8cec77b15bef7572399f144ab6
Dream <wwuwwei@126.com>
2018-09-29 18:01:29 +0800

    handle round_index -1

--- controllers/statics.js
+++ controllers/statics.js
@@ -131,6 +131,46 @@ function getRoundStatus(round_index,callback){
 		});
 }
 
+
+
+function getRoundStatus(round_index,callback){
+	if (round_index == -1){
+		db.query("SELECT max(round_index) as curRound from round", [], function(rows) {
+			round_index = curRound ;
+			getRoundStatusByRoundIndex(round_index, callback);
+	})}
+	else{
+		getRoundStatusByRoundIndex(round_index, callback);
+	}
+
+}
+
+function getRoundStatusByRoundIndex(round_index,callback){
+	var data = {};
+	db.query("SELECT \n\
+		FROM units \n\
+		WHERE round_index =? and pow_type IS NOT NULL ", [round_index], function(rows) {
+			var arrPowunits = rows.filter(function (unit){
+				return unit.pow_type == 1 ;
+			});
+			var arrTrustMEunits = rows.filter(function (unit){
+				return unit.pow_type == 2 ;
+			});
+			var arrCoinbaseunits = rows.filter(function (unit){
+				return unit.pow_type == 3 ;
+			});
+
+			data['countofPOWUnit'] = arrPowunits.length;
+			data['countofTrustMEUnit'] = arrTrustMEunits.length;
+			data['countofCoinbaseUnit'] = arrCoinbaseunits.length;
+			callback(data);
+		});
+
+}
+
+
+
+
 exports.getStatistics = getStatistics;
 exports.getRoundStatus = getRoundStatus;
 setTimeout(updateStatistics,1000*5);
